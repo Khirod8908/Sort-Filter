d5857016bce4381cf625f066b4078da93ccbdbd9
Khirod8908 <112250730+Khirod8908@users.noreply.github.com>
2022-09-20 18:07:37 +0530

    1 file modified

--- components/Filter.js
+++ components/Filter.js
@@ -8,8 +8,12 @@ const Filter = () => {
   };
 
   const sortRes = data.sort((a, b) => {
+    if (option === 'recomended') {
+      return a.id > b.id ? 1 : -1;
+    }
+
     if (option === 'popularity') {
-      return a.popularity > b.popularity ? 1 : -1;
+      return a.popularity < b.popularity ? 1 : -1;
     }
 
     if (option === 'high_to_low') {
@@ -27,7 +31,7 @@ const Filter = () => {
       {/* filtering by name */}
       <div className="select_filter">
         <select value={option} onChange={sorting}>
-          <option>All</option>
+          <option value="recomended">Recomended</option>
           <option value="popularity">Popularity</option>
           <option value="high_to_low">Price: High to Low</option>
           <option value="low_to_high">Price: Low to High</option>
@@ -35,10 +39,22 @@ const Filter = () => {
       </div>
       {sortRes.map((e, i) => {
         return (
-          <div key={i}>
-            <h6>{e.name}</h6>
-            <p>{e.price}</p>
-            <span>{e.popularity}</span>
+          <div
+            key={i}
+            style={{
+              border: '1px solid #d4d4d4',
+              padding: '.5rem',
+              width: '300px',
+              margin: '1rem auto',
+            }}
+          >
+            <h4 style={{ marginBottom: '.2rem' }}>Name: {e.name}</h4>
+            <label
+              style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}
+            >
+              <p>Price: {e.price}</p>
+              <p>Popularity: {e.popularity}</p>
+            </label>
           </div>
         );
       })}
